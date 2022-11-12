41d32a33bff3f19c40c4a92f000001eccae1e813
Nicholas Carlson <nickybrianc@gmail.com>
2022-11-12 02:06:13 -0500

    Day4 Solved!

--- 2015/DayFour.cs
+++ 2015/DayFour.cs
@@ -1,14 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdventOfCode._2015
 {
-    public class DayFour
+    public class DayFour : ISolveable
     {
-        public DayFour()
-        { }
+        private string _key;
+        
+        public DayFour(string input)
+        {
+            _key = input;
+        }
+
+        public int SolvePart1()
+        {
+            var md5 = MD5.Create();
+
+            for(var attempt = 0; attempt < 1000 * 1000 * 1; attempt++)
+            {
+                var bytes = System.Text.Encoding.ASCII.GetBytes(_key+$"{attempt}");
+                var hashed = md5.ComputeHash(bytes);
+                var output = Convert.ToHexString(hashed);
+
+                if (output.StartsWith("00000")) return attempt;
+            }
+
+            return -1;
+        }
+
+        public int SolvePart2()
+        {
+            var md5 = MD5.Create();
+
+            for (var attempt = 0; attempt < 1000 * 1000 * 1000; attempt++)
+            {
+                var bytes = System.Text.Encoding.ASCII.GetBytes(_key + $"{attempt}");
+                var hashed = md5.ComputeHash(bytes);
+                var output = Convert.ToHexString(hashed);
+
+                if (output.StartsWith("000000")) return attempt;
+            }
+
+            return -1;
+        }
     }
 }
