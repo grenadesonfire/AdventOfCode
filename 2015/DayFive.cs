07ca5a3a2ac277e22eac5a65138b64f8a6566b19
Nicholas Carlson <nickybrianc@gmail.com>
2022-11-13 10:21:57 -0500

    Day 5 and 6part1

---
+++ 2015/DayFive.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015
+{
+    public class DayFive : ISolveable
+    {
+        private string[] _possibleNiceStrings;
+
+        public DayFive(string[] niceStringCandidates)
+        {
+            _possibleNiceStrings = niceStringCandidates;
+        }
+
+        public int SolvePart1()
+        {
+            return _possibleNiceStrings.Count(s => PassesTripleTests(s));
+        }
+
+        public int SolvePart2()
+        {
+            return _possibleNiceStrings.Count(s => PassesDoubleTests(s));
+        }
+
+        private bool PassesTripleTests(string s)
+        {
+            return HasThreeVowels(s) &&
+                HasDoubleLetter(s) &&
+                HasNoneOf(s, new string[] { "ab", "cd", "pq", "xy" });
+        }
+
+        private bool PassesDoubleTests(string s)
+        {
+            return HasDouble2Grams(s) &&
+                Has3SplitPair(s);
+        }
+
+        private bool Has3SplitPair(string s)
+        {
+            for(var sIdx = 0; sIdx < s.Length - 2; sIdx++)
+            {
+                if (s[sIdx] == s[sIdx + 2]) return true;
+            }
+            return false;
+        }
+
+        private bool HasDouble2Grams(string s)
+        {
+            for(var sIdx = 0; sIdx < s.Length - 1; sIdx++)
+            {
+                var digram = s.Substring(sIdx, 2);
+                if (s.Length - sIdx >= 2 &&
+                    s.Substring(sIdx + 2, s.Length - sIdx - 2).Contains(digram))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool HasNoneOf(string s, string[] evil)
+        {
+            foreach(var e in evil)
+            {
+                if (s.Contains(e)) return false;
+            }
+            return true;
+        }
+
+        private bool HasDoubleLetter(string s)
+        {
+            var lastLetter = s[0];
+            for(var cIdx = 1; cIdx < s.Length; cIdx++)
+            {
+                if (lastLetter == s[cIdx]) return true;
+                lastLetter = s[cIdx];
+            }
+            return false;
+        }
+
+        private bool HasThreeVowels(string s)
+        {
+            return s.Count(c =>
+                c == 'a' ||
+                c == 'e' ||
+                c == 'i' ||
+                c == 'o' ||
+                c == 'u') >= 3;
+        }
+
+
+    }
+}
---
+++ 2015/DaySix.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015
+{
+    /// <summary>
+    /// There are two initial ways I thought to solve this:
+    ///     1) The naive way, just make an array of 1,000,000 and flip on or off
+    ///     2) Make an https://en.wikipedia.org/wiki/Interval_tree
+    /// </summary>
+    public class DaySix : ISolveable
+    {
+        private string[] _commands;
+
+        public DaySix(string[] commands)
+        {
+            _commands = commands;
+        }
+
+        public int SolvePart1()
+        {
+            var grid = new LightGrid(_commands);
+            grid.RunCommands();
+            return grid.On;
+        }
+
+        public int SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class LightGrid
+        {
+            private List<LightCommand> _commands;
+            private bool[,] _grid;
+            public int On { get; set; } = 0;
+
+            public LightGrid(string[] commands)
+            {
+                _commands = commands.Select(c => new LightCommand(c)).ToList();
+                _grid = new bool[1000, 1000];
+            }
+
+            public void RunCommands()
+            {
+                foreach(var cmd in _commands)
+                {
+                    Execute(cmd);
+                }
+            }
+
+            private void Execute(LightCommand cmd)
+            {
+                for(var yIdx = cmd.Top.Y; yIdx <= cmd.Bottom.Y; yIdx++)
+                {
+                    for(var xIdx = cmd.Top.X;xIdx<=cmd.Bottom.X;xIdx++)
+                    {
+                        switch (cmd.Instruction)
+                        {
+                            case LightCommandInst.OFF:
+                                if (_grid[yIdx, xIdx]) On--;
+                                _grid[yIdx, xIdx] = false;
+                                break;
+                            case LightCommandInst.ON:
+                                if (!_grid[yIdx, xIdx]) On++;
+                                _grid[yIdx, xIdx] = true;
+                                break;
+                            case LightCommandInst.TOGGLE:
+                                _grid[yIdx, xIdx] = !_grid[yIdx, xIdx];
+                                On += _grid[yIdx, xIdx] ? 1 : -1;
+                                break;
+                        }
+                    }
+                }
+            }
+
+            private enum LightCommandInst
+            {
+                ON,
+                OFF,
+                TOGGLE
+            }
+
+            private class LightCommand
+            {
+                public LightCommandInst Instruction { get; set; }
+                public Point Top { get; set; }
+                public Point Bottom { get; set; }
+
+                public LightCommand(string c)
+                {
+                    var parts = c.Split(' ');
+
+                    if(parts.Length == 5)
+                    {
+                        Instruction = parts[1] == "on" ? LightCommandInst.ON : LightCommandInst.OFF;
+                        Top = StrToPoint(parts[2]);
+                        Bottom = StrToPoint(parts[4]);
+                    }
+                    else
+                    {
+                        Instruction = LightCommandInst.TOGGLE;
+                        Top = StrToPoint(parts[1]);
+                        Bottom = StrToPoint(parts[3]);
+                    }
+                }
+
+                private Point StrToPoint(string v)
+                {
+                    var nums = v.Split(',');
+                    return new Point(int.Parse(nums[0]), int.Parse(nums[1]));
+                }
+            }
+        }
+    }
+}
