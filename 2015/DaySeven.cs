e201e4e9c31685fef1b5ac379d656b26931c3d06
Nicholas Carlson <nickybrianc@gmail.com>
2022-11-14 00:40:05 -0500

    Part 1 solved

--- 2015/DaySeven.cs
+++ 2015/DaySeven.cs
@@ -37,7 +37,8 @@ namespace AdventOfCode._2015
         private class LogicMachine
         {
             private List<Instruction> _instructions;
-            private Dictionary<string, Connection> _wires;
+            private Dictionary<string, ushort> _cache = new Dictionary<string, ushort>();
+
             private const int UNSIGNED_SHORT_MAX = (short.MaxValue * 2 + 1);
 
             public LogicMachine(List<Instruction> instructions) => _instructions = instructions;
@@ -45,24 +46,26 @@ namespace AdventOfCode._2015
             public ushort Execute(string label)
             {
                 var instr = _instructions.First(i => i.ResultLocation == label);
+                ushort checkedArg1 = LiteralOrLookup(instr.Arg1);
+                ushort checkedArg2 = LiteralOrLookup(instr.Arg2);
+
                 try
                 {
                     switch (instr.Type)
                     {
                         case InstructionType.Assignment:
-                            if (ushort.TryParse(instr.Arg1, out ushort assignTest)) return assignTest;
-                            return Execute(instr.Arg1);
+                            return checkedArg1;
                         case InstructionType.And:
-                            return (ushort)(Execute(instr.Arg1) & Execute(instr.Arg2));
+                            return (ushort)(checkedArg1 & checkedArg2);
                         case InstructionType.Or:
-                            return (ushort)(Execute(instr.Arg1) | Execute(instr.Arg2));
+                            return (ushort)(checkedArg1 | checkedArg2);
                         // Perform left shift operation, handle over flow by & with max value for short - 16 bit value
                         case InstructionType.Lshift:
-                            return (ushort)((Execute(instr.Arg1) << int.Parse(instr.Arg2)) & UNSIGNED_SHORT_MAX);
+                            return (ushort)((checkedArg1 << int.Parse(instr.Arg2)) & UNSIGNED_SHORT_MAX);
                         case InstructionType.Rshift:
-                            return (ushort)((Execute(instr.Arg1) >> int.Parse(instr.Arg2)));
+                            return (ushort)((checkedArg1 >> int.Parse(instr.Arg2)));
                         case InstructionType.Not:
-                            return (ushort)(~Execute(instr.Arg1) & UNSIGNED_SHORT_MAX);
+                            return (ushort)(~checkedArg1 & UNSIGNED_SHORT_MAX);
                         default:
                             throw new NotImplementedException("There is nothing to handle that method.");
                     }
@@ -72,6 +75,17 @@ namespace AdventOfCode._2015
                     System.Diagnostics.Debugger.Launch();
                     throw ex;
                 }
+            }
+
+            private ushort LiteralOrLookup(string arg)
+            {
+                if (string.IsNullOrEmpty(arg)) return 0;
+                if (ushort.TryParse(arg, out var literal)) return literal;
+                if (_cache.ContainsKey(arg)) return _cache[arg];
+                // This means do a lookup if its not a literal
+                var ret = Execute(arg);
+                _cache.Add(arg, ret);
+                return ret;
             }
         }
 
