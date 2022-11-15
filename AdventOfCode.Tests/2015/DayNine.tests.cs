8e8a2f48503e4c51154fbaa34aab77543f1f9c93
Nicholas Carlson <nickybrianc@gmail.com>
2022-11-15 13:00:16 -0500

    Moved around folders and added a test project, Day9 also added

---
+++ AdventOfCode.Tests/2015/DayNine.tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests._2015
+{
+    public class DayNine
+    {
+        [Fact]
+        public void PassesGivenTestCase()
+        {
+            var input = new string[]
+            {
+                "London to Dublin = 464",
+                "London to Belfast = 518",
+                "Dublin to Belfast = 141"
+            };
+
+            var day = new AdventOfCode._2015.DayNine(input);
+
+            Assert.Equal(605, day.SolvePart1());
+        }
+    }
+}
---
+++ AdventOfCode/2015/DayNine.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015
+{
+    public class DayNine : ISolveable
+    {
+        private List<Route> _routes;
+        private List<string> _locations;
+
+        public DayNine(string[] input)
+        {
+            _routes = input.Select(s => new Route(s)).ToList();
+            _locations = _routes.Select(r => r.Location1).ToList();
+            _locations.AddRange(_routes.Select(r => r.Location2));
+            _locations = _locations.Distinct().ToList();
+        }
+
+        public int SolvePart1()
+        {
+            return StartDFS();
+        }
+
+        private int StartDFS()
+        {
+            var min = int.MaxValue;
+
+            foreach(var place in _locations)
+            {
+                var visited = new List<string> { place };
+
+                var minRoute = DFS(visited, place);
+
+                if (minRoute < min) min = minRoute;
+            }
+
+            return min;
+        }
+
+        private int DFS(List<string> visited, string currentlyAt)
+        {
+            var left = _locations.Where(l => !visited.Contains(l));
+
+            if (left.Count() == 0) return 0;
+
+            var min = int.MaxValue;
+            var best = string.Empty;
+
+            foreach(var place in left)
+            {
+                //Create the new list so we don't mess up other branches
+                // could also use a queue or stack but ehhh.
+                var newVisit = visited.ToList();
+                newVisit.Add(place);
+
+                var dist = DFS(newVisit, place);
+
+                if(dist < min)
+                {
+                    min = dist;
+                    best = place;
+                }
+            }
+
+            return min + LookupDistance(currentlyAt, best);
+        }
+
+        private int LookupDistance(string currentlyAt, string place)
+        {
+            return _routes
+                .FirstOrDefault(s =>
+                    (s.Location1 == currentlyAt || s.Location1 == place) &&
+                    (s.Location2 == currentlyAt || s.Location2 == place))
+                .Distance;
+        }
+
+        public int SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class Route
+        {
+            public string Location1 { get; set; }
+            public string Location2 { get; set; }
+            public int Distance { get; set; }
+
+            public Route(string s)
+            {
+                var splits = s.Split(new char[] { ' ', '=' }, StringSplitOptions.RemoveEmptyEntries);
+                Location1 = splits[0];
+                Location2 = splits[2];
+                Distance = int.Parse(splits[3]);
+            }
+        }
+    }
+}
